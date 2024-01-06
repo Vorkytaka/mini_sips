8fefecc4b06472ee2254ce8bb8c7da5174a7a932
Vorkytaka <vorkytaka@gmail.com>
2024-01-07 01:00:38 +0300

    #25: Some improvements

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/alcohol.dart';
+import '../data_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,7 +21,7 @@ class HomeScreen extends StatelessWidget {
               additionalDividerMargin: 0,
               backgroundColor: Colors.transparent,
               children: const [
-                Text('123'),
+                PreviousDrinkWidget(),
               ],
             ),
           ),
@@ -51,6 +55,29 @@ class CupertinoCard extends StatelessWidget {
     final startWidget = isLtr ? bottomStart : bottomEnd;
     final endWidget = isLtr ? bottomEnd : bottomStart;
 
+    final Widget? bottom;
+    if (startWidget != null || endWidget != null) {
+      bottom = Padding(
+        padding: const EdgeInsetsDirectional.only(
+          top: 4,
+          start: 20.0,
+          end: 14.0,
+          bottom: 12,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (startWidget != null) Expanded(child: startWidget),
+            if (endWidget != null) Expanded(child: endWidget),
+          ],
+        ),
+      );
+    } else {
+      bottom = null;
+    }
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -71,16 +98,31 @@ class CupertinoCard extends StatelessWidget {
           ),
           trailing: trailing,
         ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (startWidget != null) Expanded(child: startWidget),
-            if (endWidget != null) Expanded(child: endWidget),
-          ],
-        ),
+        if (bottom != null) bottom,
       ],
+    );
+  }
+}
+
+class PreviousDrinkWidget extends StatelessWidget {
+  const PreviousDrinkWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<DataCubit, DataState, Alcohol?>(
+      selector: (state) =>
+          state.alcohol.isNotEmpty ? state.alcohol.first : null,
+      builder: (context, alcohol) {
+        if(alcohol == null) {
+          return SizedBox();
+        }
+
+        return CupertinoCard(
+          icon: Icon(Icons.undo),
+          title: Text('Предыдущий'),
+          bottomStart: Text('В последний раз вы пили ${alcohol!.datetime.toIso8601String()}'),
+        );
+      },
     );
   }
 }
